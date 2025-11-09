d68788ce67590763e23f6f61f2a71ab1a8efbdd4
JEAN YVES GATWAZA <jgatwazakubwimana@college.harvard.edu>
2025-11-09 18:05:43 -0500

    Fix the ci tests

--- app.js
+++ app.js
@@ -77,7 +77,11 @@ app.post('/fetch', async (req, res) => {
   }
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Faleproxy server running at http://localhost:${PORT}`);
-});
+// Start the server only if this file is run directly
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Faleproxy server running at http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
